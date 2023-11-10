2ba3e0b1270d3d9948f44f95e967f3a76755feb0
arsh1018 <artem.sheveliukhin@netcracker.com>
2023-11-10 17:43:04 +0300

    save meaning as a face of a card

--- main.py
+++ main.py
@@ -5,43 +5,48 @@ import os
 from clean import clean_all
 from create_image import create_image_collage
 
+
 def clean_words_txt():
     with open("words.txt", 'w', encoding='utf-8') as wfile:
         wfile.write('')
+
 
 def get_sound_word(sound_url, word, headers):
     response_sound = requests.get(sound_url, headers=headers)
     with open(word + '.mp3', 'wb') as f:
         f.write(response_sound.content)
 
+
 def parser_reverso(headers, reverso_dict_url, word):
     word_url = reverso_dict_url + word
     response_word = requests.get(word_url, headers=headers)
-    soup = BeautifulSoup(response_word.text, "lxml") #html.parser
+    soup = BeautifulSoup(response_word.text, "lxml")  # html.parser
     data = soup.find("section", id='examples-content')
     data_example_sentence = data.find("div", class_="example")
-    example_sentence = data_example_sentence.find("div", class_= "src ltr").text.strip()
+    example_sentence = data_example_sentence.find("div", class_="src ltr").text.strip()
     return example_sentence
+
 
 def perser_cambrige(headers, cambridge_base_url, cambridge_dict_url, word, reverso_dict_url):
     print('The word "' + word + '" is loaded...')
     word_url = cambridge_dict_url + word
     response_word = requests.get(word_url, headers=headers)
-    soup = BeautifulSoup(response_word.text, "lxml") #html.parser
-    data = soup.find("div", class_ = 'pr entry-body__el')
-    
-    type_word = data.find("span", class_ = "pos dpos").text
-    definition = data.find("div", class_ = "def ddef_d db").text
+    soup = BeautifulSoup(response_word.text, "lxml")  # html.parser
+    data = soup.find("div", class_='pr entry-body__el')
+
+    type_word = data.find("span", class_="pos dpos").text
+    definition = data.find("div", class_="def ddef_d db").text
     try:
-        example_sentence = data.find("span", class_ = "eg deg").text
+        example_sentence = data.fin("span", class_="eg deg").text
     except AttributeError:
         example_sentence = parser_reverso(headers, reverso_dict_url, word)
-    pronanciation = data.find("span", class_ = "ipa dipa lpr-2 lpl-1").text 
-    sound_url = cambridge_base_url + data.find("source", type = "audio/mpeg")['src']
+    pronunciation = data.find("span", class_="ipa dipa lpr-2 lpl-1").text
+    sound_url = cambridge_base_url + data.find("source", type="audio/mpeg")['src']
 
     get_sound_word(sound_url, word, headers)
 
-    return type_word, definition, example_sentence, pronanciation
+    return type_word, definition, example_sentence, pronunciation
+
 
 def main():
     cambridge_base_url = 'https://dictionary.cambridge.org'
@@ -53,35 +58,57 @@ def main():
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;Win64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
     }
 
-    my_model = genanki.Model(
-    1686681943,
-    'Basic',
-    fields=[
-        {'name': 'word'},
-        {'name': 'type'},
-        {'name': 'ipa'},
-        {'name': 'meaning'},
-        {'name': 'example'},
-        {'name': 'sound'},
-        {'name': 'pic'},
-    ],
-    templates=[
-        {
-        'name': 'Card 1',
-        'qfmt': "<div style='font-family: Arial; font-size: 60px;color:#FF80DD;'>{{word}}{{sound}}</div><hr><div style='font-family: Arial; font-size: 25px;color:#79005E;'>{{type}}</div><div style='font-family: Arial; font-size: 25px;color:#AB2B52;'>{{ipa}}</div>",
-        'afmt': "<div id=answer style='font-family: Arial; color:#FF80DD;'>{{FrontSide}}</div><hr><div  style='font-family: Arial; color:#00aaaa; text-align:left;'>Meaning: {{meaning}}</div><hr><div  style='font-family: Arial; color:#9CFFFA; text-align:left;'>&nbsp;→&nbsp;Example: {{example}}</div><hr>{{pic}}",
-        },
-    ], css= '.card {font-family: arial; font-size: 20px; text-align: center; color: black; background-color: black;}')
+    word_model = genanki.Model(
+        1686681943,
+        'Basic',
+        fields=[
+            {'name': 'word'},
+            {'name': 'type'},
+            {'name': 'ipa'},
+            {'name': 'meaning'},
+            {'name': 'example'},
+            {'name': 'sound'},
+            {'name': 'pic'},
+        ],
+        templates=[
+            {
+                'name': 'Card 1',
+                'qfmt': "<div style='font-family: Arial; font-size: 60px;color:#FF80DD;'>{{word}}{{sound}}</div><hr><div style='font-family: Arial; font-size: 25px;color:#79005E;'>{{type}}</div><div style='font-family: Arial; font-size: 25px;color:#AB2B52;'>{{ipa}}</div>",
+                'afmt': "<div id=answer style='font-family: Arial; color:#FF80DD;'>{{FrontSide}}</div><hr><div  style='font-family: Arial; color:#00aaaa; text-align:left;'>Meaning: {{meaning}}</div><hr><div  style='font-family: Arial; color:#9CFFFA; text-align:left;'>&nbsp;→&nbsp;Example: {{example}}</div><hr>{{pic}}",
+            },
+        ],
+        css='.card {font-family: arial; font-size: 20px; text-align: center; color: black; background-color: black;}')
+
+    meaning_model = genanki.Model(
+        1686681943,
+        'Basic',
+        fields=[
+            {'name': 'word'},
+            {'name': 'type'},
+            {'name': 'ipa'},
+            {'name': 'meaning'},
+            {'name': 'example'},
+            {'name': 'sound'},
+            {'name': 'pic'},
+        ],
+        templates=[
+            {
+                'name': 'Card 1',
+                'qfmt': "<div style='font-family: Arial; font-size: 60px;color:#FF80DD;'>{{meaning}}</div><hr><div style='font-family: Arial; font-size: 25px;color:#79005E;'>{{type}}</div>",
+                'afmt': "<div id=answer style='font-family: Arial; color:#FF80DD;'>{{FrontSide}}</div><hr><div  style='font-family: Arial; color:#00aaaa; text-align:left;'>Word: {{word}}</div><div  style='font-family: Arial; color:#00aaaa; text-align:left;'>IPA: {{ipa}}</div><div style='font-family: Arial; font-size: 25px;color:#AB2B52;'>{{sound}}</div><hr><div  style='font-family: Arial; color:#9CFFFA; text-align:left;'>&nbsp;→&nbsp;Example: {{example}}</div><hr>{{pic}}",
+            },
+        ],
+        css='.card {font-family: arial; font-size: 20px; text-align: center; color: black; background-color: black;}')
 
     my_deck = genanki.Deck(
-    1686686578,
-    'automatic_dictionary')
-    
-    my_package = genanki.Package(my_deck)    
+        1686686578,
+        'automatic_dictionary')
+
+    my_package = genanki.Package(my_deck)
     problem_words = []
 
     if os.path.exists('words.txt') == False:
-        print('"words.txt" does not exist') 
+        print('"words.txt" does not exist')
     elif os.stat('words.txt').st_size == 0:
         print('"words.txt" list is empty')
     else:
@@ -90,7 +117,10 @@ def main():
         for word in lines:
             if word:
                 try:
-                    type_word, definition, example_sentence, pronanciation = perser_cambrige(headers, cambridge_base_url, cambridge_dict_url, word, reverso_dict_url)
+                    type_word, definition, example_sentence, pronunciation = perser_cambrige(headers,
+                                                                                             cambridge_base_url,
+                                                                                             cambridge_dict_url, word,
+                                                                                             reverso_dict_url)
                 except AttributeError:
                     problem_words.append(word)
                     print("Parsing Error\n")
@@ -99,12 +129,19 @@ def main():
                 if create_image_collage(word=word, headers=headers) == False:
                     problem_words.append(word)
                     print("Image Error\n")
-                    continue                    
+                    continue
 
-                my_note = genanki.Note(
-                model=my_model,
-                fields=[word, type_word, pronanciation, definition, example_sentence, "[sound:" + word + ".mp3]", "<img src='collage_" + word + ".jpg'>"])
-                
+                face = 'meaning'
+                if face == 'meaning':
+                    my_note = genanki.Note(
+                        model=meaning_model,
+                        fields=[word, type_word, pronunciation, definition, example_sentence,
+                                "[sound:" + word + ".mp3]", "<img src='collage_" + word + ".jpg'>"])
+                else:
+                    my_note = genanki.Note(
+                        model=word_model,
+                        fields=[word, type_word, pronunciation, definition, example_sentence,
+                                "[sound:" + word + ".mp3]", "<img src='collage_" + word + ".jpg'>"])
                 my_deck.add_note(my_note)
                 my_package.media_files.append(word + '.mp3')
                 my_package.media_files.append('collage_' + word + '.jpg')
@@ -119,10 +156,10 @@ def main():
             print("Problem words did't find")
 
         my_package.write_to_file('output.apkg')
-        clean_words_txt()
+        # clean_words_txt()
         clean_all()
         print("The program is finished")
-    
+
+
 if __name__ == "__main__":
     main()
-
